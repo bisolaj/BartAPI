90aca8c664d7ef200c7a28dc6d4bfa2df2ce4594
Code <code@COCTL-41288.local>
2020-01-21 13:55:52 -0800

    First Commit

---
+++ BARTAPI/src/app/main/main.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  apiURL = 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=Z2R9-PHQ2-9JWT-DWE9&json=y'
+  constructor(private dataS: DataService) { }
+
+  ngOnInit() {
+    console.log(this.apiURL);
+    this.dataS.getURL(this.apiURL).subscribe(
+      x => {
+        
+        console.log(x);
+      }
+    )
+  }
+
+  
+
+}
